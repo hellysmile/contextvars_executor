024b6f9e2c6b092bcc2719f71d2860e5cd6476ec
hellysmile <hellysmile@gmail.com>
2018-09-22 09:35:41 +0300

    Added args test.

--- tests/test_asyncio.py
+++ tests/test_asyncio.py
@@ -44,3 +44,17 @@ async def test_future(loop, executor):
     ret = await asyncio.ensure_future(loop.run_in_executor(executor, sync))
 
     assert ret == 42
+
+
+async def test_args(loop, executor):
+
+    ctx.set(42)
+
+    def sync(foo):
+        assert ctx.get() == 42
+
+        return foo
+
+    ret = await loop.run_in_executor(executor, sync, 42)
+
+    assert ret == 42
