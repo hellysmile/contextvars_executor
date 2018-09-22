a229ed017d562b1b0cc51d80922f0219f8804f38
hellysmile <hellysmile@gmail.com>
2018-09-22 09:36:43 +0300

    Added kwargs tests

--- tests/test_threads.py
+++ tests/test_threads.py
@@ -12,3 +12,16 @@ def test_basic(executor):
     fut = executor.submit(sync)
 
     assert fut.result() == 42
+
+
+def test_kwargs(executor):
+    ctx.set(42)
+
+    def sync(*, foo):
+        assert ctx.get() == 42
+
+        return foo
+
+    fut = executor.submit(sync, foo=42)
+
+    assert fut.result() == 42
